064c3d3646d2b8c30775fcd6bbf749ff0cb3f0d7
alan.tai <alan.tai@JSA-190350.local>
2024-06-12 20:45:15 -0700

    fix brew installs fzf tab completion and add font, kubectl, coreutils, lazygit

--- brew_installs.sh
+++ brew_installs.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+brew install kubectl
+brew install coreutils
+
+brew install font-hack-nerd-font
 brew install bash bash-completion@2
 brew install gnu-sed
 # Ripgrep is a faster grep alternative
@@ -14,7 +18,9 @@ brew install gawk grep gnu-sed coreutils
 # Install FZF and set it up w/ tab completion support
 brew install fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-git clone git clone https://github.com/lincheney/fzf-tab-completion ~/fzf-tab-completion/
+git clone https://github.com/lincheney/fzf-tab-completion ~/fzf-tab-completion/
 ~/.fzf/install # Install tab completion
 brew install jandedobbeleer/oh-my-posh/oh-my-posh
 brew install --cask linearmouse
+
+brew install nvim
